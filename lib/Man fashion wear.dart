d366f7b3f09ac788839878269718ddfa9e0ded51
adwin <adwinlamal@gmil.com>
2021-07-10 16:34:43 +0545

    Third

--- lib/Man fashion wear.dart
+++ lib/Man fashion wear.dart
@@ -7,6 +7,11 @@ class Fashion extends StatefulWidget {
 }
 
 class _FashionState extends State<Fashion> {
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -14,14 +19,34 @@ class _FashionState extends State<Fashion> {
         child: Container(
           width: MediaQuery.of(context).size.width*1,
           height: MediaQuery.of(context).size.height*1,
-          child: Column(
+          child: ListView(
             children: [
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Men's Fashion",style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold,
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(1.0, 1.0),
+                              blurRadius: 1.0,
+                              color: Colors.grey,
+                            ),
+                          ],
 
+                        ),),
+                      ),
+
+
+                    ],
+                  )),
 
               Container(
-                margin: EdgeInsets.only(top: 8),
+                margin: EdgeInsets.all(10),
                 height: MediaQuery.of(context).size.height*0.28,
-                width: MediaQuery.of(context).size.width*0.95,
+                width: MediaQuery.of(context).size.width*9,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.redAccent,
@@ -96,7 +121,7 @@ class _FashionState extends State<Fashion> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Hoody",style: TextStyle(color: Colors.white)),
+                              child: Text("Hoodies",style: TextStyle(color: Colors.white)),
                             )),
                       ),
                       Padding(
@@ -114,7 +139,7 @@ class _FashionState extends State<Fashion> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Hoody",style: TextStyle(color: Colors.white)),
+                              child: Text("Jeans Shorts",style: TextStyle(color: Colors.white)),
                             )),
                       ),
                       Padding(
@@ -132,7 +157,7 @@ class _FashionState extends State<Fashion> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Hoody",style: TextStyle(color: Colors.white)),
+                              child: Text("Blazer",style: TextStyle(color: Colors.white)),
                             )),
                       ),
                       Padding(
@@ -150,7 +175,7 @@ class _FashionState extends State<Fashion> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Hoody",style: TextStyle(color: Colors.white),),
+                              child: Text("Shoes",style: TextStyle(color: Colors.white),),
                             )),
                       ),
                       Padding(
@@ -168,7 +193,7 @@ class _FashionState extends State<Fashion> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Hoody",style: TextStyle(color: Colors.white)),
+                              child: Text("Watches",style: TextStyle(color: Colors.white)),
                             )),
                       ),
                       Padding(
@@ -186,13 +211,133 @@ class _FashionState extends State<Fashion> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Hoody",style: TextStyle(color: Colors.white)),
+                              child: Text("Sun Glasses",style: TextStyle(color: Colors.white)),
                             )),
                       ),
                     ],
                   ),
                 ),
-              )
+              ),
+
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Trends",style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(1.0, 1.0),
+                            blurRadius: 1.0,
+                            color: Colors.grey,
+                          ),
+                        ],
+
+                      ),),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0,1),
+                                blurRadius: 2
+                            )]
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text("See More",style: TextStyle(color: Colors.deepOrangeAccent,fontWeight: FontWeight.normal,
+                            ),),
+                          ),
+                        ),
+                      ),
+                    ),
+
+
+                  ],
+                )),
+
+              Container(
+                margin: EdgeInsets.all(10),
+                height: MediaQuery.of(context).size.height*0.28,
+                width: MediaQuery.of(context).size.width*0.95,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.redAccent,
+                    boxShadow: [BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0,1),
+                        blurRadius: 2
+                    )]
+                ),
+
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("New",style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold,
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(1.0, 1.0),
+                              blurRadius: 1.0,
+                              color: Colors.grey,
+                            ),
+                          ],
+
+                        ),),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [BoxShadow(
+                                  color: Colors.grey,
+                                  offset: Offset(0,1),
+                                  blurRadius: 2
+                              )]
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Center(
+                              child: Text("See More",style: TextStyle(color: Colors.deepOrangeAccent,fontWeight: FontWeight.normal,
+                              ),),
+                            ),
+                          ),
+                        ),
+                      ),
+
+
+                    ],
+                  )),
+
+              Container(
+                margin: EdgeInsets.all(10),
+                height: MediaQuery.of(context).size.height*0.28,
+                width: MediaQuery.of(context).size.width*0.95,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.redAccent,
+                    boxShadow: [BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0,1),
+                        blurRadius: 2
+                    )]
+                ),
+
+              ),
+
 
             ],
           ),

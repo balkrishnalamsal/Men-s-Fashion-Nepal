2f23445c2dc7bdb6dd26121552916186b9876ab5
adwin <adwinlamal@gmail.com>
2021-11-26 15:45:08 +0545

    Quantity completed

--- lib/ViewDetails.dart
+++ lib/ViewDetails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
 class ViewDetails extends StatefulWidget {
@@ -10,31 +11,47 @@ class ViewDetails extends StatefulWidget {
 class _ViewDetailsState extends State<ViewDetails> {
 
   String ? color= "Blue";
+  int ? value =01;
+
+  colorchanger(){
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: (color=="Blue")?Colors.blueAccent:(color=="Red")?Colors.red:Colors.green,
+    ));
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    colorchanger();
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor:(color=="Blue")?Colors.blueAccent:(color=="Red")?Colors.red:Colors.green,
         body: ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Icon(CupertinoIcons.arrow_left,color: Colors.white,)
-                ],
+            GestureDetector(
+              onTap: (){
+               Navigator.pop(context);
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15.0,top: 10,right: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(CupertinoIcons.arrow_left,color: Colors.white,size: 30,),
+                    Icon(CupertinoIcons.cart,color: Colors.white,size: 30,)
+                  ],
+                ),
               ),
             ),
 
         Container(
-          color: Colors.blueAccent,
+          color: (color=="Blue")?Colors.blueAccent:(color=="Red")?Colors.red:Colors.green,
           height: MediaQuery.of(context).size.height*0.4,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.only(left: 20,bottom: 10),
                 child: Text("Office",style: TextStyle(color: Colors.white,fontSize: 25),),
               ),
 
@@ -65,125 +82,220 @@ class _ViewDetailsState extends State<ViewDetails> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text("Color",style: TextStyle(color: Colors.grey),),
+                    padding: const EdgeInsets.only(top: 15.0,left: 25),
+                    child: Text("Color",style: TextStyle(color: Colors.blue,fontSize: 15),),
                   ),
 
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: GestureDetector(
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        GestureDetector(
                           onTap: (){
                             setState(() {
                               color="Blue";
                             });
 
                           },
-                          child: Container(
-                            height: 20,
-                            width: 20,
-                       decoration:(color=="Blue")?BoxDecoration(
-                              boxShadow: [BoxShadow(
-                                color: Colors.grey,
-                                blurRadius: 1,
-                                offset: Offset(
-                                  0,1
-                                )
-                              )],
-                              border: Border.all(
-                                width: 2,color: Colors.white
-                              ),
-                            shape: BoxShape.circle,
-                            color: Colors.white
-                            ):BoxDecoration(
-                           border: Border.all(width: 2,color: Colors.white),
-                           shape: BoxShape.circle,
-                       ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 15.0),
                             child: Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.blue
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: GestureDetector(
-                          onTap: (){
-                            setState(() {
-                              color="Green";
-                            });
-                          },
-                          child: Container(
-                            height: 20,
-                            width: 20,
-                            decoration:(color=="Green")?BoxDecoration(
+                              height: 30,
+                              width: 30,
+                           decoration:(color=="Blue")?BoxDecoration(
                                 boxShadow: [BoxShadow(
-                                    color: Colors.grey,
-                                    blurRadius: 1,
-                                    offset: Offset(
-                                        0,1
-                                    )
+                                  color: Colors.grey,
+                                  blurRadius: 1,
+                                  offset: Offset(
+                                    0,1
+                                  )
                                 )],
                                 border: Border.all(
-                                    width: 2,color: Colors.white
+                                  width: 2,color: Colors.white
                                 ),
-                                shape: BoxShape.circle,
-                                color: Colors.white
-                            ):BoxDecoration(
-                              border: Border.all(width: 2,color: Colors.white),
-                                shape: BoxShape.circle,
-                            ),
-                            child: Container(
-                              decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white
+                              ):BoxDecoration(
+                             border: Border.all(width: 2,color: Colors.white),
+                             shape: BoxShape.circle,
+                           ),
+                              child: Container(
+                                decoration: BoxDecoration(
                                   shape: BoxShape.circle,
-                                  color: Colors.green
+                                  color: Colors.blue
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: GestureDetector(
-                          onTap: (){
-                            setState(() {
-                              color="Red";
-                            });
-                          },
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                color="Green";
+                              });
+                            },
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration:(color=="Green")?BoxDecoration(
+                                  boxShadow: [BoxShadow(
+                                      color: Colors.grey,
+                                      blurRadius: 1,
+                                      offset: Offset(
+                                          0,1
+                                      )
+                                  )],
+                                  border: Border.all(
+                                      width: 2,color: Colors.white
+                                  ),
+                                  shape: BoxShape.circle,
+                                  color: Colors.white
+                              ):BoxDecoration(
+                                border: Border.all(width: 2,color: Colors.white),
+                                  shape: BoxShape.circle,
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.green
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                color="Red";
+                              });
+                            },
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration:(color=="Red")?BoxDecoration(
+                                  boxShadow: [BoxShadow(
+                                      color: Colors.grey,
+                                      blurRadius: 1,
+                                      offset: Offset(
+                                          0,1
+                                      )
+                                  )],
+                                  border: Border.all(
+                                      width: 2,color: Colors.white
+                                  ),
+                                  shape: BoxShape.circle,
+                                  color: Colors.white
+                              ):BoxDecoration(
+                                border: Border.all(width: 2,color: Colors.white),
+                                  shape: BoxShape.circle,
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.red
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    height: MediaQuery.of(context).size.height
+                    *0.2,
+                    decoration: BoxDecoration(
+                      color:Colors.red
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Quantity"),
+                  ),
+
+                  Container(
+                    height: MediaQuery.of(context).size.height*0.1,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                if(value!>=1){
+                                  value=(value!-1);
+                                }
+                              });
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [BoxShadow(
+                                  color: Colors.grey,
+                                  blurRadius: 2,
+                                  offset: Offset(0,1)
+                                )]
+                              ),
+                              child: Icon(
+                                CupertinoIcons.minus,color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
                           child: Container(
-                            height: 20,
-                            width: 20,
-                            decoration:(color=="Red")?BoxDecoration(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
                                 boxShadow: [BoxShadow(
                                     color: Colors.grey,
-                                    blurRadius: 1,
-                                    offset: Offset(
-                                        0,1
-                                    )
-                                )],
-                                border: Border.all(
-                                    width: 2,color: Colors.white
-                                ),
-                                shape: BoxShape.circle,
-                                color: Colors.white
-                            ):BoxDecoration(
-                              border: Border.all(width: 2,color: Colors.white),
-                                shape: BoxShape.circle,
+                                    blurRadius: 2,  offset: Offset(0,1)
+                                )]
                             ),
+                            child: Center(child: Text("$value",style: TextStyle(fontSize: 15),)),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                value=(value!+1);
+                              });
+                            },
                             child: Container(
+                              height: 40,
+                              width: 40,
                               decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.red
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [BoxShadow(
+                                      color: Colors.grey,
+                                      blurRadius: 2,  offset: Offset(0,1)
+                                  )]
+                              ),
+                              child: Icon(
+                                CupertinoIcons.add,color: Colors.black,
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ],
+                        )
+                      ],
+                    ),
                   )
 
                 ],
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(CupertinoApp(
+    color: Colors.blueAccent,
     debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
